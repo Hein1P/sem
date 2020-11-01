9eea4457b8f0738392f9f6313cba26476a959e75
Hein Thu <40478652@live.napier.ac.uk>
2020-11-01 20:59:56 +0630

    app modify

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,9 +16,9 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Chalmers")
+        Document doc = new Document("name", "Hein Thu")
                 .append("class", "Software Engineering Methods")
-                .append("year", "2018/19")
+                .append("year", "2020/21")
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
