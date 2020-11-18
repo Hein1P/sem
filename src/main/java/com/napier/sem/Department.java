76de9e517d127ded85c8f26c77d43f81e2a1f3c2
Hein Thu <40478652@live.napier.ac.uk>
2020-11-18 14:16:51 +0630

    Modify all

---
+++ src/main/java/com/napier/sem/Department.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class Department {
+
+    public int dept_no;
+
+    public String dept_name;
+
+    public Employee manager;
+}
---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,53 @@
+package sem;
+
+
+import com.napier.sem.App;
+import com.napier.sem.Employee;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printSalariesTestNull()
+    {
+        app.printSalaries(null);
+    }
+    @Test
+    void printSalariesTestEmpty()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        app.printSalaries(employees);
+    }
+    @Test
+    void printSalariesTestEmptyMem()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        employees.add(null);
+        app.printSalaries(employees);
+    }
+    @Test
+    void printSalariesTestNormal()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "hein";
+        emp.last_name = "thu";
+        employees.add(emp);
+        app.printSalaries(employees);
+    }
+}
